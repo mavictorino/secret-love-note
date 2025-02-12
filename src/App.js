c6d4a392e6abb9fa6ee7ef6851f1c670c387f464
Mariana Victorino <victorino.marii@gmail.com>
2025-02-12 19:07:11 +0000

    add ViewNote

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NoteForm from './components/NoteForm';
+import NoteCreated from './components/NoteCreated';
+import ViewNote from './pages/ViewNote';
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [noteId, setNoteId] = useState(null);
+
   return (
-    <div className="App">
-      <h1>Secret love note</h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={!noteId ? <NoteForm onNoteCreated={setNoteId}/> : <NoteCreated noteId={noteId} />} />
+        <Route path='/note/:id' element={<ViewNote />} /> 
+      </Routes>
+    </Router>
+  )
+    
 }
 
 export default App;
---
+++ src/pages/ViewNote.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { db } from "../services/firebaseConfig";
+import { doc, getDoc, deleteDoc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+
+const ViewNote = () => {
+    const { id } = useParams();
+    const [passcode, setPasscode] = useState("");
+    const [message, setMessage] = useState(null);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        setMessage(null);
+        setError("");
+    }, [id]);
+
+    const handleUnlock = async () => {
+        const docRef = doc(db, "notes", id);
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+            setError("Ops, this note doesn't exist!");
+            return;
+        }
+
+        const data = docSnap.data();
+        if (data.passcode !== passcode) {
+            setError("Incorrect passcode. Try again!");
+            return;
+        }
+
+        setMessage(data.message);
+
+        await deleteDoc(docRef);
+    };
+
+    return (
+        <div>
+            {message ? (
+                <div>
+                    <h3>💌 Your secret love message:</h3>
+                    <p>{message}</p>
+                </div>
+            ) : (
+                <div>
+                    <p>Enter passcode to unlock:</p>
+                    <input
+                        type="password"
+                        value={passcode}
+                        onChange={(e) => setPasscode(e.target.value)}
+                    />
+                    <button onClick={handleUnlock}>Unlock 💖</button>
+                    {error && <p style={{ color: "red" }}>{error}</p>}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ViewNote;
