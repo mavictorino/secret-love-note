5ae6f38ad889ea4b0981a79989cef2f2ea295ef1
Mariana Victorino <victorino.marii@gmail.com>
2025-02-14 19:03:58 +0000

    add tostify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NoteForm from './components/NoteForm';
 import NoteCreated from './components/NoteCreated';
 import ViewNote from './pages/ViewNote';
 import { initHeartCursor } from './components/HeartCursor';
+import { ToastContainer } from "react-toastify";
+
 import { useState, useEffect } from 'react';
 
 import './App.css';
@@ -10,20 +12,22 @@ import './App.css';
 function App() {
 
   const [noteId, setNoteId] = useState(null);
-  
+
   useEffect(() => {
     initHeartCursor();
   }, []);
 
   return (
+
     <Router>
+      <ToastContainer />
       <Routes>
-        <Route path='/' element={!noteId ? <NoteForm onNoteCreated={setNoteId}/> : <NoteCreated noteId={noteId} />} />
-        <Route path='/note/:id' element={<ViewNote />} /> 
+        <Route path='/' element={!noteId ? <NoteForm onNoteCreated={setNoteId} /> : <NoteCreated noteId={noteId} />} />
+        <Route path='/note/:id' element={<ViewNote />} />
       </Routes>
     </Router>
   )
-    
+
 }
 
 export default App;
--- src/components/NoteCreated.js
+++ src/components/NoteCreated.js
@@ -1,13 +1,35 @@
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const NoteCreated = ({ noteId }) => {
     const noteLink = `${window.location.origin}/note/${noteId}`;
+
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                toast.success("Link copied to clipboard!", {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    theme: "light",
+                });
+            })
+            .catch((err) => {
+                toast.error("Failed to copy!", {
+                    position: "top-right",
+                });
+                console.error("Copy failed:", err);
+            });
+    };
 
     return (
         <div className="note-created">
             <h2>Your secret love note is ready! 🎉</h2>
             <input type="text" value={noteLink} readOnly />
-            <button onClick={() => navigator.clipboard.writeText(noteLink)}>Copy link 🔗</button>
+            <button onClick={() => copyToClipboard(noteLink)}>Copy link 🔗</button>
         </div>
     );
 };
